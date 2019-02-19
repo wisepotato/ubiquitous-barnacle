7e7cc3f40d8a6fde71ed0fb5a710f3f0ede05a4a
Maarten Ackermans <maarten.ackermans@gmail.com>
2019-02-19 21:14:02 +0100

    test(go-euler-1-maarten): refactor test cases initialization

--- go/euler/1/maarten/main_test.go
+++ go/euler/1/maarten/main_test.go
@@ -5,19 +5,17 @@ import (
 	"testing"
 )
 
-type testCase struct {
-	multiples      []int
-	maxValue       int
-	expectedResult int
-}
-
 // TestSumMultiples ...
 func TestSumMultiples(t *testing.T) {
-	testCases := []*testCase{
-		&testCase{[]int{3}, 5, 3},
-		&testCase{[]int{5}, 24, 50},
-		&testCase{[]int{3, 5}, 10, 23},
-		&testCase{[]int{3, 5}, 20, 78},
+	testCases := []struct {
+		multiples      []int
+		maxValue       int
+		expectedResult int
+	}{
+		{[]int{3}, 5, 3},
+		{[]int{5}, 24, 50},
+		{[]int{3, 5}, 10, 23},
+		{[]int{3, 5}, 20, 78},
 	}
 
 	for i, tc := range testCases {
