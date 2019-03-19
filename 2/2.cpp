de151050e3e9810b9228e81aaa8fe2422f9d5364
Harro van der Kroft <wisepotato@users.noreply.github.com>
2019-03-19 09:55:44 +0100

    Update 2.cpp

--- 2/2.cpp
+++ 2/2.cpp
@@ -3,23 +3,22 @@
 #include <math.h>     
 using namespace std;
 
-	int main()
-	{
-		long long int fn2 = 0, fn1 = 2;
-		long int sum = fn2 + fn1;
-		long int fn;
-		long int limit = 4 * pow(10,6);
-		while (sum <= limit) {
-			fn = 4 * fn1 + fn2;
-			sum += fn;
+int main()
+{
+	long long int fn2 = 0, fn1 = 2;
+	long int sum = fn2 + fn1;
+	long int fn;
+	long int limit = 4 * pow(10,6);
+	while (sum <= limit) {
+		fn = 4 * fn1 + fn2;
+		sum += fn;
 
-			fn2 = fn1;
-			fn1 = fn;
-		}
-
-		cout << sum;
-		int returnvalue;
-		cin >> returnvalue;
-		return 0;
+		fn2 = fn1;
+		fn1 = fn;
 	}
 
+	cout << sum;
+	int returnvalue;
+	cin >> returnvalue;
+	return 0;
+}
